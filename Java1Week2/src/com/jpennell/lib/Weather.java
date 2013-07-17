079d5ec69bd4a3fa1ec5f7f0f26ccb5ae185d3ad
Jerry Pennell <jwpenn@fullsail.edu>
2013-07-17 19:27:14 -0400

    Added weather object
    Getters and setters for zip for location for weather object

---
+++ Java1Week2/src/com/jpennell/lib/Weather.java
@@ -0,0 +1,30 @@
+/*
+ * project		Java1Week2
+ * 
+ * package		com.jpennell.lib
+ * 
+ * author		Jerry Pennell
+ * 
+ * date			Jul 17, 2013
+ */
+package com.jpennell.lib;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface Weather.
+ */
+public interface Weather {
+	/**
+	 * Sets the zip.
+	 *
+	 * @param zip the zip
+	 * @return true, if successful
+	 */
+	public boolean setZip(String zip);
+	/**
+	 * Gets the zip.
+	 *
+	 * @return the zip
+	 */
+	public String getZip();
+}
